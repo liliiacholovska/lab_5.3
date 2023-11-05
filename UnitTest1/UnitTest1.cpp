17b2a63b7e7713775d197ef1b260e67c451e2b3b
Ліля <chlvskalilia@gmail.com>
2023-11-05 18:46:25 +0200

    first test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.3/lab5.3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double x = 2.0;
+			double expected = (exp(x) + sin(x)) / ((pow(cos(x), 2) + 1));
+
+			double actual = k(x);
+
+			Assert::AreEqual(expected, actual, 0.001);
+		}
+	};
+}
